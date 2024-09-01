aef1552307a51b506ce8515bb8541aefc83b4be9
Aj-ay-yadav <ajay.kdic@gmail.com>
2024-09-01 18:15:35 +0530

    commit message

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>jjkfjfjh</h1>
+       <h1>hello react</h1>
       </header>
     </div>
   );
